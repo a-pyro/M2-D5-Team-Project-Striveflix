77560bb1e93a4f7212670784c04e6615e6ba2c9c
ardigermenji <ardi.germenji@gmail.com>
2021-02-19 18:24:19 +0100

    fixes layout and add getuser to whos watching

--- whos-watching.js
+++ whos-watching.js
@@ -4,6 +4,23 @@ const shit = '💩',
   rocket = '🚀',
   poudzo = '👍🏻';
 
+// user
+let watcher = null;
 const imgsDom = document.querySelectorAll('.row img');
+const editProfileBtn = document.getElementById('editProfileBtn');
 
 imgsDom.forEach((img, idx) => (img.src = `prof-pic/${idx}.png`));
+
+editProfileBtn.addEventListener('click', triggerEdit);
+
+imgsDom.forEach((img) => img.addEventListener('click', setUser));
+function setUser(e) {
+  e.preventDefault();
+  const source = e.target.src;
+  const watcher = source.slice(source.indexOf('prof-pic'));
+  console.log(watcher);
+}
+
+function triggerEdit(e) {
+  console.log(e.target);
+}
